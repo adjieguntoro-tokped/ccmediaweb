93a146c7914499679f2e6faefc9f2e32a58bfcf4
Adjie Guntoro <adjie.guntoro@tokopedia.com>
2021-09-14 22:30:11 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,16 @@
-import '../styles/globals.css'
+// import 'bootstrap/dist/css/bootstrap.css';
+import 'tailwindcss/tailwind.css'
+// import '../styles/style.css';
+// import '../styles/bootsnav.css';
+// import '../styles/animate.css';
+// import '../styles/responsive.css';
+// import '../styles/plugins.min.css';
+// import '../styles/owl.theme.default.min.css';
+// import '../styles/owl.carousel.min.css';
+// import '../styles/font-awesome.min.css';
+// import '../styles/magnific-popup.css';
+// import '../styles/flaticon-set.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
